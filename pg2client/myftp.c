e836b7f6373939aa5afc7a883f1dbdf6a7867e32
Chris Foley <cfoley220@gmail.com>
2019-10-02 21:50:48 -0400

    lots of work done

--- pg2client/myftp.c
+++ pg2client/myftp.c
@@ -22,43 +22,26 @@
 
 #define MAX_BUFFER_SIZE 4096
 
-// send public key to the server (sentto() function)
+void list_handler(int);
+void mkdr_handler(int);
+void rmdr_handler(int);
+void chdr_handler(int);
+void crfl_handler(int);
+void rmfl_handler(int);
 
-int send_short(short value, int socket)
-{
-   	int len;
-	short temp = htonl(value);
-	if ((len = write(socket, &temp, sizeof(temp))) == -1) {
-		perror("ERROR: Client Send");
-		exit(1);
-	}
-
-	return len;
-}
-
-int receive_short(int s) {
-	int buffer;
-	int len;
-	if ((len = read(s, &buffer, sizeof(buffer))) == -1) {
-		perror("ERROR: Client Receive");
-		exit(1);
-	}
-
-	short temp = ntohl(buffer);
-	return temp;
-}
 
 int send_buffer(int s, char* buffer, int size) {
 	int len;
-    if ((len = write(s, buffer, size)) == -1) {
+  if ((len = write(s, buffer, size)) == -1) {
         perror("ERROR: Client Send\n");
         exit(1);
-    }
+  }
 	return len;
 }
 
 int receive_buffer(int s, char* buffer, int size) {
     int len;
+    bzero(buffer, sizeof(buffer));
     if ((len = read(s, buffer, size)) == -1) {
         perror("ERROR: Client Receive!");
         exit(1);
@@ -80,6 +63,7 @@ int send_int(int value, int s) {
 int receive_int(int s) {
     int buffer;
     int len;
+    bzero(&buffer, sizeof(buffer));
     if ((len = read(s, &buffer, sizeof(buffer))) == -1) {
         perror("ERROR: Client Receive");
         exit(1);
@@ -110,8 +94,7 @@ int main(int argc, char *argv[]) {
   servaddr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr*) gethostbyname("student02.cse.nd.edu")->h_addr_list[0]))); // ip addr of student02
   servaddr.sin_port = htons(41030);
 	// servaddr.sin_port = htons(41015);
-
-  char messageBuffer[MAX_BUFFER_SIZE];
+	// servaddr.sin_port = htons(41002);
 
   // connect
   if (connect(clientSocket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
@@ -121,111 +104,76 @@ int main(int argc, char *argv[]) {
 
   printf("Client: Connected\n");
 
-	char *op;
+	char *userInput;
 
 	while(1) {
 
-		// Prompt the user for an operation
-		printf("Operations: DNLD: Download, UPLD: Upload, LIST: List, MKDR: Make Directory, RMDR: Remove Directory, CHDR: Change Directory, CRFL: Create File, RMFL: Remove File, QUIT: Quit\n");
+		// Prompt the user for an Operation
+		printf("Operations:\n\tDNLD: Download,\n\tUPLD: Upload,\n\tLIST: List,\n\tMKDR: Make Directory,\n\tRMDR: Remove Directory,\n\tCHDR: Change Directory,\n\tCRFL: Create File,\n\tRMFL: Remove File,\n\tQUIT: Quit\n\n");
 		printf("Enter an operation: ");
-		fgets(op, 50, stdin);
+		fgets(userInput, 50, stdin);
 
-		// get rid of the endline character
-		op[strlen(op)-1] = '\0';
+		// Get rid of the endline character
+		userInput[strlen(userInput)-1] = '\0';
 
-	  //printf("Sending the message\n");
+    // Get operation out of user input
+    char *operation = strtok(userInput, " "); // TODO: regex for any whitespace?
+    // TODO: ENSURE THAT OPERATION IS 4 CHARACTERS
+    // currently errors out: to reproduce:
+    // run server and client
+    // on client:
+    // send: "MKDIR aa"
+    // send: "MKDR aa"
 
-	  // send the operation to the server
-	  // int sent = sendto(clientSocket, op, 50, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-	  //int sent = send_buffer(clientSocket, op, strlen(op));
+	  printf("Sending the operation\n");
 
-	  //printf("Sent the message, %d characters\n", sent);
+	  // Send the operation to the server
+	  int sent = send_buffer(clientSocket, operation, strlen(operation));
+    // TODO: double check that strlen works
+
+	  printf("Sent the operation, %d bytes sent\n", sent);
 
 		// DNLD: Download
-		if (strcmp(op, "DNLD") == 0) {
+		if (strcmp(operation, "DNLD") == 0) {
 			printf("Sent DNLD command\n");
 
 		}
 		// UPLD: Upload
-		else if (strcmp(op, "UPLD") == 0) {
+		else if (strcmp(operation, "UPLD") == 0) {
 			printf("Sent UPLD command\n");
 		}
 		// LIST: List
-		else if (strcmp(op, "LIST") == 0) {
-
-			int sent = send_buffer(clientSocket, op, strlen(op));
-			printf("Sent the message, %d characters\n", sent);
-			//printf("Sent LIST command\n");
-
-			// Get size of directory
-			int directorySize = receive_int(clientSocket);
-			//printf("Size of directory: %d\n", directorySize);
-
-			// Recieve listing
-			int bytesRecvd = 0;
-			while(bytesRecvd < directorySize) {
-				memset(&messageBuffer, 0, sizeof(messageBuffer));
-				bytesRecvd += receive_buffer(clientSocket, messageBuffer, sizeof(messageBuffer));
-				//printf("Bytes received: %d\n", bytesRecvd);
-				//printf("Msg buffer: %s\n", messageBuffer);
-				printf("%s\n", messageBuffer);
-
-				// Escape reading loop if its empty or error
-				if (bytesRecvd <= 0){
-					break;
-				}
-			}
-			fflush(stdout);
+		else if (strcmp(operation, "LIST") == 0) {
+			printf("Sent LIST command\n");
+      list_handler(clientSocket);
 		}
 		// MKDR: Make Directories
-		else if (strstr(op, "MKDR ") != 0) {
-			printf("Sent MKDR command\n");
-			char* command = "MKDR";
-			int sent = send_buffer(clientSocket, command, strlen(command));
-			printf("Sent the message, %d characters\n", sent);
-
-			char* dir_name = op + 5; //get directory name
-			int dir_length = strlen(dir_name);
-			printf("%s %d\n", dir_name, dir_length);
-
-			sent = send_int(dir_length, clientSocket); //send size
-			sent = send_buffer(clientSocket, dir_name, strlen(dir_name)); //send name
-
-			int status = receive_int(clientSocket);
-			if (status == -2) {
-				perror("ERROR: The directory already exists on the server.\n");
-			}
-			else if (status == -1) {
-				perror("ERROR: Making directory.\n");
-			}
-			else {
-				printf("The directory was successfully made!\n");
-			}
+		else if (strcmp(operation, "MKDR") == 0) {
+      printf("Sent MKDR command\n");
+			mkdr_handler(clientSocket);
 		}
 		// RMDR: Remove Directory
-		else if (strcmp(op, "RMDR") == 0) {
+		else if (strcmp(operation, "RMDR") == 0) {
 			printf("Sent RMDR command\n");
-
+      rmdr_handler(clientSocket);
 		}
 		// CHDR: Change Directory
-		else if (strcmp(op, "CHDR") == 0) {
+		else if (strcmp(operation, "CHDR") == 0) {
 			printf("Sent CHDR command\n");
-
+      chdr_handler(clientSocket);
 		}
 		// CRFL: Create File
-		else if (strcmp(op, "CRFL") == 0) {
-			//printf("Sent CRFL command\n");
-
-
-
+		else if (strcmp(operation, "CRFL") == 0) {
+			printf("Sent CRFL command\n");
+      crfl_handler(clientSocket);
 		}
 		// RMFL: Remove File
-		else if (strcmp(op, "RMFL") == 0) {
+		else if (strcmp(operation, "RMFL") == 0) {
 			printf("Sent RMFL command\n");
-
+      rmfl_handler(clientSocket);
 		}
-
-		else if (strcmp(op, "QUIT") == 0){
+    // QUIT: Quit
+		else if (strcmp(operation, "QUIT") == 0){
 			break;
 		}
 
@@ -238,4 +186,230 @@ int main(int argc, char *argv[]) {
 	close(clientSocket);
 
 	// TODO: catch a ^C on the client side and sent a message to the server to shut down
+}
+
+void list_handler(int clientSocket){
+  char messageBuffer[MAX_BUFFER_SIZE];
+
+  // Get size of directory
+  int directorySize = receive_int(clientSocket);
+  //printf("Size of directory: %d\n", directorySize);
+
+  // Recieve listing
+  int bytesRecvd = 0;
+  while(bytesRecvd < directorySize) {
+    memset(&messageBuffer, 0, sizeof(messageBuffer));
+    bytesRecvd += receive_buffer(clientSocket, messageBuffer, sizeof(messageBuffer));
+    //printf("Bytes received: %d\n", bytesRecvd);
+    //printf("Msg buffer: %s\n", messageBuffer);
+    printf("%s\n", messageBuffer);
+
+    // Escape reading loop if its empty or error
+    if (bytesRecvd <= 0){
+      break;
+    }
+  }
+}
+
+void mkdr_handler(int clientSocket) {
+	// Get directory name and size
+	char* directory = strtok(NULL, " ");
+
+	// Send length of directory name (short int)
+	int received = send_int(strlen(directory), clientSocket);
+
+	// Send directory name (string)
+	send_buffer(clientSocket, directory, strlen(directory));
+
+	// Recieve status update and inform user
+	int status = receive_int(clientSocket);
+
+	if (status == -2) {
+		printf("ERROR: The directory already exists on the server.\n");
+	}
+	else if (status == -1) {
+		printf("ERROR: Making directory.\n");
+	}
+	else {
+		printf("The directory was successfully made!\n");
+	}
+
+}
+
+void rmdr_handler(int clientSocket){
+  // Get directory name and size
+  char* directory = strtok(NULL, " ");
+
+  printf("Directory is: %s\n", directory);
+
+  // Send length of directory name (short int)
+  int received = send_int(strlen(directory), clientSocket);
+
+  // Send directory name (string)
+  send_buffer(clientSocket, directory, strlen(directory));
+
+  // Recieve status update and inform user
+  int status = receive_int(clientSocket);
+
+  printf("Recieved status: %d\n", status);
+
+  if (status == -1) {
+    // Directory DNE
+    printf("ERROR: The directory does not exist on server.\n");
+  }
+  else if (status == -2) {
+    // Directory not empty
+    printf("ERROR: The directory is not empty.\n");
+  }
+  else if (status == 1){
+    // Directory can be deleted
+
+    // Get user confirmation
+    printf("Are you sure you would like to delete %s?\n", directory);
+
+    char userConfirmation[5];
+    bzero(userConfirmation, sizeof(userConfirmation));
+    fgets(userConfirmation, 5, stdin);
+
+    // Get rid of the endline character
+    userConfirmation[strlen(userConfirmation)-1] = '\0';
+    // Ensure last two characters are null
+    userConfirmation[4] = '\0';
+    userConfirmation[5] = '\0';
+
+    // Send confirmation
+    send_buffer(clientSocket, userConfirmation, 5);
+
+
+    if (strcmp(userConfirmation, "Yes") == 0) {
+        // User wants to delete
+        printf("Waiting for status\n");
+        int status = receive_int(clientSocket);
+        printf("Recieved status back: %d\n", status);
+
+        if (status > 0) {
+          printf("Directory deleted\n");
+        } else if (status < 0) {
+          printf("Failed to delete directory\n");
+        } else {
+          printf("ERROR: Unknown status received for RMDR from server.\n");
+        }
+    } else {
+        // User does not want to delete
+      printf("Delete abandoned by the user!\n");
+    }
+
+  } else {
+    printf("ERROR: Unknown status received for RMDR from server.\n");
+  }
+
+  printf("ENDING RMDR PART\n");
+}
+
+void chdr_handler(int clientSocket){
+  // Get directory name and size
+  char* directory = strtok(NULL, " ");
+
+  printf("Directory is: %s\n", directory);
+
+  // Send length of directory name (short int)
+  int received = send_int(strlen(directory), clientSocket);
+
+  // Send directory name (string)
+  send_buffer(clientSocket, directory, strlen(directory));
+
+  // Recieve status update and inform user
+  int status = receive_int(clientSocket);
+
+  if (status > 0) {
+    printf("Changed current directory.\n");
+  } else if (status == -1) {
+    printf("ERROR: Error in changing directory\n");
+  } else if (status == -2) {
+    printf("ERROR: The directory does not exist on server.\n");
+  } else {
+    printf("ERROR: Unknown status received for CHDR from server.\n");
+  }
+}
+
+void crfl_handler(int clientSocket) {
+  // Get file name and size
+  char* filename = strtok(NULL, " ");
+
+  printf("File is: %s\n", filename);
+
+  // Send length of directory name (short int)
+  send_int(strlen(filename), clientSocket);
+
+  // Send filename (string)
+  printf("sent size\n");
+  send_buffer(clientSocket, filename, strlen(filename));
+
+  // Recieve status update and inform user
+  int status = receive_int(clientSocket);
+
+  if (status > 0) {
+    printf("The file was successfully created.\n");
+  } else if (status < 0) {
+    printf("ERROR: The file already exists.\n");
+  } else {
+    printf("ERROR: Unknown status received for CRFL from server.\n");
+  }
+}
+
+void rmfl_handler(int clientSocket) {
+  // Get file name and size
+  char* filename = strtok(NULL, " ");
+
+  printf("File is: %s\n", filename);
+
+  // Send length of directory name (short int)
+  int received = send_int(strlen(filename), clientSocket);
+
+  // Send filename (string)
+  send_buffer(clientSocket, filename, strlen(filename));
+
+  // Recieve status update and inform user
+  int status = receive_int(clientSocket);
+
+  if (status < 0) {
+    printf("The file does not exist on server.\n");
+  } else if (status > 0) {
+    // Get user confirmation
+    printf("Are you sure you would like to delete %s?\n", filename);
+
+    char userConfirmation[5];
+    bzero(userConfirmation, sizeof(userConfirmation));
+    fgets(userConfirmation, 5, stdin);
+
+    // Get rid of the endline character
+    userConfirmation[strlen(userConfirmation)-1] = '\0';
+    // Ensure last two characters are null
+    userConfirmation[4] = '\0';
+    userConfirmation[5] = '\0';
+
+    // Send confirmation
+    send_buffer(clientSocket, userConfirmation, 5);
+
+    if (strcmp(userConfirmation, "Yes") == 0) {
+        // User wants to delete
+        printf("Waiting for status\n");
+        int status = receive_int(clientSocket);
+        printf("Recieved status back: %d\n", status);
+
+        if (status > 0) {
+          printf("File deleted\n");
+        } else if (status < 0) {
+          printf("Failed to delete file\n");
+        } else {
+          printf("ERROR: Unknown status received for RMFL from server.\n");
+        }
+    } else {
+        // User does not want to delete
+      printf("Delete abandoned by the user!\n");
+    }
+
+  } else {
+    printf("ERROR: Unknown status received for RMFL from server.\n");
+  }
 }
--- pg2server/myftpd.c
+++ pg2server/myftpd.c
@@ -23,32 +23,40 @@
 #include <errno.h>
 #include <unistd.h>
 
-
 #define MAX_BUFFER_SIZE 4096
+#define EMPTY_DIRECTORY_SIZE 6
+#define EMPTY_FILE_SIZE 0
 
 void list_handler(int);
-void mkdir_handler(int);
+void mkdr_handler(int);
+void rmdr_handler(int);
+void chdr_handler(int);
+void crfl_handler(int);
+void rmfl_handler(int);
 
-int send_short(short value, int socket) {
-    int len;
-    short temp = htonl(value);
-    if ((len = write(socket, &temp, sizeof(temp))) == -1) {
-        perror("ERROR: Client Send");
-        exit(1);
-    }
-
-    return len;
-}
+// int send_short(short value, int socket) {
+//     int len;
+//     short temp = htons(value);
+//     if ((len = write(socket, &temp, sizeof(temp))) == -1) {
+//         perror("ERROR: Client Send");
+//         exit(1);
+//     }
+//
+//     return len;
+// }
 
 int receive_short(int s) {
-  int buffer;
+  printf("IN SHORT FUNCTION\n");
+  char buffer[MAX_BUFFER_SIZE];
   int len;
-  if ((len = read(s, &buffer, sizeof(buffer))) == -1) {
+  bzero(buffer, sizeof(buffer));
+  printf("Size we are waiting to receive %d\n", sizeof(short));
+  if ((len = read(s, buffer, sizeof(short))) == -1) {
       perror("ERROR: Client Receive");
       exit(1);
   }
 
-  short temp = ntohl(buffer);
+  short temp = ntohs(*buffer);
   return temp;
 }
 
@@ -63,6 +71,7 @@ int send_buffer(int s, char* buffer, int size) {
 
 int receive_buffer(int s, char* buffer, int size) {
     int len;
+    bzero(buffer, sizeof(buffer));
     if ((len = read(s, buffer, size)) == -1) {
         perror("ERROR: Receive Error!");
         exit(1);
@@ -85,6 +94,7 @@ int receive_int(int s) {
     printf("IN FUNCTION\n");
     int buffer;
     int len;
+    // bzero(&buffer, sizeof(buffer));
     if ((len = read(s, &buffer, sizeof(buffer))) == -1) {
         perror("ERROR: Client Receive");
         exit(1);
@@ -102,6 +112,7 @@ int main(int argc, char* argv[]) {
 
     int port = 41030;
     // int port = 41015;
+    // int port = 41002;
 
 
     /* Initialize variables */
@@ -144,13 +155,9 @@ int main(int argc, char* argv[]) {
       fprintf(stderr, "%s: Failed to listen: %s\n", argv[0], strerror(errno));
       exit(-1);
     }
-    // TESTING CHDIR
-    //printf("changing directory!\n");
-    //execvp("/bin/ls", "ls");
-    //printf("changed directory\n");
 
     printf("Listened\n");
-    printf("awaiting connection\n");
+    printf("Awaiting connection\n");
     int clientSocket;
     if((clientSocket = accept(sockfd, (struct sockaddr *) &clientAddr, &clientAddrLen)) < 0){
       fprintf(stderr, "%s: Failed to accept: %s\n", argv[0], strerror(errno));
@@ -159,15 +166,15 @@ int main(int argc, char* argv[]) {
     printf("accepted a connection\n");
 
     // Receive first client operation
-    printf("Receiving client operation\n");
-    char buf[4096];
+    printf("Receiving client operation (oustside loop)\n");
+    char buf[MAX_BUFFER_SIZE];
     memset(buf, 0, sizeof(buf));
     socklen_t addr_len = sizeof(clientAddr);
-    int n_received = receive_buffer(clientSocket, buf, 4096);
+    int n_received = receive_buffer(clientSocket, buf, 4);
     printf("Received client operation: %d, %s\n", n_received, buf);
 
-    int count = 0;
-    while (strcmp(buf, "QUIT") != 0 && n_received >= 0 && count++ < 5) {
+    while (strcmp(buf, "QUIT") != 0 && n_received >= 0) {
+        printf("beginning processing while loop\n");
 
       // DNLD: Download
       if (strcmp(buf, "DNLD") == 0) {
@@ -187,39 +194,39 @@ int main(int argc, char* argv[]) {
       // MKDR: Make Directory
       else if (strcmp(buf, "MKDR") == 0) {
         printf("Received MKDR command\n");
-        mkdir_handler(clientSocket);
+        mkdr_handler(clientSocket);
 
       }
       // RMDR: Remove Directory
       else if (strcmp(buf, "RMDR") == 0) {
         printf("Received RMDR command\n");
-
+        rmdr_handler(clientSocket);
       }
       // CHDR: Change Directory
       else if (strcmp(buf, "CHDR") == 0) {
         printf("Received CHDR command\n");
-
+        chdr_handler(clientSocket);
       }
       // CRFL: Create File
       else if (strcmp(buf, "CRFL") == 0) {
         printf("Received CRFL command\n");
-
-
-
+        crfl_handler(clientSocket);
       }
       // RMFL: Remove File
       else if (strcmp(buf, "RMFL") == 0) {
         printf("Received RMFL command\n");
+        rmfl_handler(clientSocket);
 
       }
       // Default case
       else {
-        printf("Recieved unknown command\n");
+        printf("Received unknown command\n");
         // TODO: Handle this
       }
 
+      printf("Finished processing command, waiting for another operation\n");
       memset(buf, 0, sizeof(buf));
-      int n_received = receive_buffer(clientSocket, buf, 4096);
+      int n_received = receive_buffer(clientSocket, buf, 4);
       printf("Received client operation: %d, %s\n", n_received, buf);
 
     }
@@ -247,7 +254,7 @@ void list_handler(int clientSocket){
   // Obtain list of directory
   fp = popen("ls -l", "r");
 
-  // Determine size of directoy
+  // Determine size of directory
   while((bytesRead = fread(buffer, sizeof(char), sizeof(buffer)-1, fp)) > 0) {
     totalBytesRead += bytesRead;
   }
@@ -256,7 +263,7 @@ void list_handler(int clientSocket){
   // Send size of directory
   send_int(totalBytesRead, clientSocket);
 
-  // Read directoy listing
+  // Read directory listing
   char directoryListing[totalBytesRead];
   fp = popen("ls -l", "r");
   //printf("Did fseek work?: %d\n\t %s\n", val, strerror(errno));
@@ -277,29 +284,144 @@ void list_handler(int clientSocket){
 
 }
 
-void mkdir_handler(int clientSocket) {
-    char* dirBuffer;
+void mkdr_handler(int clientSocket) {
+    // Prepare to recieve directory
+    char dirBuffer[MAX_BUFFER_SIZE];
+    bzero((char*)&dirBuffer, sizeof(dirBuffer));
+
+    // Recieve size and directory name
     int directory_size = receive_int(clientSocket);
-    printf("Received directory size %d\n", directory_size);
-    int bytesReceived = receive_buffer(clientSocket, dirBuffer, strlen(dirBuffer));
-    printf("Received name length %d\n", bytesReceived);
+    int bytesReceived = receive_buffer(clientSocket, dirBuffer, directory_size);
 
     struct stat st;
 
+    // Check if directory already exists
     if (stat(dirBuffer, &st) == 0 && S_ISDIR(st.st_mode)) {
-        printf("Directory already exists!\n");
         int sent = send_int(-2, clientSocket);
     }
     else {
-        printf("Trying to create directory\n");
+        // Create directory
         if (mkdir(dirBuffer, 0777) == -1) {
-            perror("ERROR: Failed to create directory.\n");
             int sent = send_int(-1, clientSocket);
         }
         else {
             int sent = send_int(1, clientSocket);
-            printf("Directory created!\n");
         }
-            
     }
+}
+
+void rmdr_handler(int clientSocket) {
+    // Prepare to recieve directory
+    char dirBuffer[MAX_BUFFER_SIZE];
+    bzero((char*)&dirBuffer, sizeof(dirBuffer));
+
+    // Recieve size and directory name
+    int directory_size = receive_int(clientSocket);
+    int bytesReceived = receive_buffer(clientSocket, dirBuffer, directory_size);
+
+    struct stat st;
+
+    // Check if directory already exists
+    if (stat(dirBuffer, &st) == 0 && S_ISDIR(st.st_mode)) {
+        if (st.st_size == EMPTY_DIRECTORY_SIZE) {
+            int sent = send_int(1, clientSocket);
+
+            char confBuffer[5];
+            bzero((char*)&confBuffer, sizeof(confBuffer));
+            int received = receive_buffer(clientSocket, confBuffer, sizeof(confBuffer));
+            if (strcmp(confBuffer, "Yes") == 0) {
+                int status = rmdir(dirBuffer);
+                if (status == 0) {
+                  sent = send_int(1, clientSocket);
+                }
+                else {
+                  sent = send_int(-1, clientSocket);
+                }
+            }
+        } else {
+          int sent = send_int(-2, clientSocket);
+        }
+    }
+    else {
+      printf("Directory DNE\n");
+        // server sends negative confirmation back to the client
+        int sent = send_int(-1, clientSocket);
+    }
+}
+
+void chdr_handler(int clientSocket) {
+    int size = receive_int(clientSocket);
+    char nameBuffer[MAX_BUFFER_SIZE];
+    bzero(nameBuffer, sizeof(nameBuffer));
+    int bytesReceived = receive_buffer(clientSocket, nameBuffer, size);
+
+    int status = chdir(nameBuffer);
+    if (status < 0) {
+      if (errno == ENOENT) {
+        int sent = send_int(-2, clientSocket);
+      }
+      else {
+        int sent = send_int(-1, clientSocket);
+      }
+    } else {
+      int sent = send_int(1, clientSocket);
+    }
+
+}
+
+void crfl_handler(int clientSocket){
+  // Prepare to recieve file
+  char fileBuffer[MAX_BUFFER_SIZE];
+  bzero((char*)&fileBuffer, sizeof(fileBuffer));
+
+  // Recieve size and file name
+  int file_size = receive_int(clientSocket);
+  int bytesReceived = receive_buffer(clientSocket, fileBuffer, file_size);
+
+  struct stat st;
+
+  // Check if file already exists
+  if (stat(fileBuffer, &st) != 0) { // File DNE
+      // Create file
+      FILE* fp;
+      fp = fopen(fileBuffer, "w");
+      fclose(fp);
+      int sent = send_int(1, clientSocket);
+  }
+  else { // File exists
+    int sent = send_int(-1, clientSocket);
+  }
+}
+
+void rmfl_handler(int clientSocket){
+  // Prepare to recieve file
+  char fileBuffer[MAX_BUFFER_SIZE];
+  bzero((char*)&fileBuffer, sizeof(fileBuffer));
+
+  // Recieve size and file name
+  int file_size = receive_int(clientSocket);
+  int bytesReceived = receive_buffer(clientSocket, fileBuffer, file_size);
+
+  struct stat st;
+
+  // Check if file already exists
+  if (stat(fileBuffer, &st) == 0 && S_ISREG(st.st_mode)) {
+    int sent = send_int(1, clientSocket);
+    char confBuffer[5];
+    bzero((char*)&confBuffer, sizeof(confBuffer));
+    int received = receive_buffer(clientSocket, confBuffer, sizeof(confBuffer));
+    if (strcmp(confBuffer, "Yes") == 0) {
+        int status = remove(fileBuffer);
+        if (status == 0) {
+          sent = send_int(1, clientSocket);
+        }
+        else {
+          sent = send_int(-1, clientSocket);
+        }
+    }
+  }
+  else {
+      // server sends negative confirmation back to the client
+    int sent = send_int(-1, clientSocket);
+  }
 }
